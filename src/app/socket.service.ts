f36f5ee00ad74ce646854b31741de4800be412a4
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-09-09 14:18:19 +0300

    Update socket.service.ts

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
+  private socket: Socket;
+  private readonly url: string = 'http://localhost:3000'; // URL of your backend
 
-  constructor(private socket: Socket) { }
+  constructor() {
+    this.socket = io(this.url);
+  }
 
-  sendMessage(message: string) {
-    this.socket.emit('sendMessage', message);
+  joinRoom(room: string): void {
+    this.socket.emit('joinRoom', room);
+  }
+
+  sendMessage(message: string, room: string): void {
+    this.socket.emit('sendMessage', message, room);
+  }
+
+  onMessage(): Observable<string> {
+    return new Observable(observer => {
+      this.socket.on('message', (message: string) => {
+        observer.next(message);
+      });
+    });
+  }
 }
-
- getMessages() {
-   return this.socket.fromEvent<string>('message');
- }
-}
-
